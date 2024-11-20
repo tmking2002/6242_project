491a95a82c52ce58e9610112d38c02ea026d5e36
tmking2002 <tmking4@ncsu.edu>
2024-11-20 15:31:39 -0500

    fixed palestine data

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ data = pd.read_csv('palestine_sentiment.csv')
 
 # randomly select half of the rows
 
-data = data.sample(frac=0.5)
+# data = data.sample(frac=0.5)
 
-data.to_csv('palesine_sentiment.csv', index=False)
+# sort by date (created column)
+data['created'] = pd.to_datetime(data['created'])
+
+data = data.sort_values(by='created')
+
+data.to_csv('palestine_sentiment.csv', index=False)
